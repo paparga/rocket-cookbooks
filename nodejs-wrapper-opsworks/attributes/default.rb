7acb0b47f1e55abd2a9d17ea9f04eb5a8dc924e1
Pablo Parga <pargag@gmail.com>
2016-06-21 11:03:38 -0400

    Node 6

--- nodejs-wrapper-opsworks/attributes/default.rb
+++ nodejs-wrapper-opsworks/attributes/default.rb
@@ -1,11 +1,11 @@
 
 # Override the version to 0.12
-default['nodejs']['version'] = '4.2.2'
+default['nodejs']['version'] = '6.2.2'
 
 # Override the repo
 case node['platform_family']
 when 'debian'
-  default['nodejs']['repo']      = 'https://deb.nodesource.com/node_4.x'
+  default['nodejs']['repo']      = 'https://deb.nodesource.com/node_6.x'
 end
 
 default["nodebin"]["location"] = '/usr/bin/nodejs'
